9547721564b98f408e01640461cbdb49f1222ad4
Kyle Scott <kyle.j.scott@hotmail.com>
2020-07-07 17:26:06 -0400

    do some functional refactoring

--- main.py
+++ main.py
@@ -1,21 +1,37 @@
 import pyautogui
-import time
+from time import sleep
 
 def main():
 
+	initialization()
+	countdown()
+	openUIElement('space', 1)
+	reportMousePosition()
+
+
+def initialization():
 	pyautogui.FAIlSAFE = True
 
+
+def countdown():
 	print("Starting in 10 seconds: ", end="")
 	for i in range(0, 10):
 		print(".", end="")
-		time.sleep(1)
+		sleep(1)
 	print("Started")
 
-	pyautogui.press("space")
-	time.sleep(5)
-	pyautogui.press("space")
 
-	print("Done!")
+def openUIElement(key, seconds=1.00):
+	# this isn't useful in my case but cool to have for now
+	pyautogui.press(key)
+	sleep(seconds)
+	pyautogui.press(key)
+
+
+def reportMousePosition(seconds=10):
+	for i in range(0, seconds):
+		print(pyautogui.position())
+		time.sleep(1)
 
 if __name__ == "__main__":
 	main()
